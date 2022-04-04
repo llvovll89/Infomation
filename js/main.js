d04b298fab88740e559028b1bbdef538a1a02999
llvovll89 <svvvs5579@naver.com>
2022-04-04 16:55:58 +0900

    16:55

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 // 전역변수 안쓰려고,,
 (() => {
+  const actions = {
+    birdFlies(key) {
+      // key 가 true면
+      if (key) {
+        document.querySelector(
+          `[data-index="2"] .bird`
+        ).style.transform = `translateX(${window.innerWidth}px)`;
+      } else {
+        document.querySelector(
+          `[data-index="2"] .bird`
+        ).style.transform = `translateX(-100%}px)`;
+      }
+    },
+    birdFlies2(key) {
+      // key 가 true면
+      if (key) {
+        document.querySelector(
+          `[data-index="5"] .bird`
+        ).style.transform = `translate(${window.innerWidth}px, ${
+          -window.innerHeight * 0.7
+        }px )`;
+      } else {
+        document.querySelector(
+          `[data-index="5"] .bird`
+        ).style.transform = `translateX(-100%}px)`;
+      }
+    },
+  };
+
   const stepElems = document.querySelectorAll(".step");
   const graphicElems = document.querySelectorAll(".graphic-item");
   // 현재 활성화된 (visible 클래스 붙은) .graphic-item지정
@@ -17,18 +46,23 @@
     graphicElems[i].dataset.index = i;
   }
 
-  function activate() {
+  function activate(action) {
     currentItem.classList.add("visible");
+    if (action) {
+      actions[action](true);
+    }
   }
 
-  function inActivate() {
+  function inActivate(action) {
     currentItem.classList.remove("visible");
+    if (action) {
+      actions[action](false);
+    }
   }
 
   window.addEventListener("scroll", () => {
     let step;
     let boundingRect;
-    let temp = 0;
 
     // for (let i = 0; i < stepElems.length; i++) {
     for (let i = ioindex - 1; i < ioindex + 2; i++) {
@@ -37,17 +71,21 @@
       // boundingRect - 속성값 ex) 위치값
       boundingRect = step.getBoundingClientRect();
 
-      temp++;
-
       if (
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
         inActivate();
         currentItem = graphicElems[step.dataset.index];
-        activate();
+        activate(currentItem.dataset.action);
       }
     }
   });
+
+  window.addEventListener("load", () => {
+    // x, y 방향 순서
+    setTimeout(() => scrollTo(0, 0), 100);
+  });
+
   activate();
 })();
